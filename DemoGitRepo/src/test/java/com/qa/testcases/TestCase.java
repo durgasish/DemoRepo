d9a49b78ff6b56987e23c18366e74fdcdd67fc94
manis <manis@192.168.1.102>
2018-11-01 15:38:14 +1100

    First Code

---
+++ DemoGitRepo/src/test/java/com/qa/testcases/TestCase.java
@@ -0,0 +1,15 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase {
+	
+	
+	@Test 
+	
+	public void logintest()
+	{
+		System.out.println("login test case passed");
+	}
+
+}
